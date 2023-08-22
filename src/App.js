4642b4dd006febbb9c4fc70e671e500ff01a3c41
Silvan Moura <silvancortesmoura@outlook.com>
2023-08-22 14:56:13 -0300

    Installing react-router and setting initial routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Componentes
+import Header from '../src/components/Header';
+import ScreenLogin from '../src/components/ScreenLogin';
+import ScreenRegister from '../src/components/ScreenRegister';
+import SearchProduct from '../src/components/SearchProduct';
+import ProductInfo from '../src/components/ProductInfo';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route path="/login" component={ScreenLogin} />
+          <Route path="/register" component={ScreenRegister} />
+          <Route path="/search" component={SearchProduct} />
+          <Route path="/Info" component={ProductInfo} />
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

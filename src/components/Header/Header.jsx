65d041299aad32c569db97a8ea5afdd19b91a377
Silvan Moura <silvancortesmoura@outlook.com>
2023-08-22 15:57:53 -0300

    Header structure and style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,18 @@
 import "./Header.css";
 const Header = () => {
-    return <div>Header</div>;
+    return <div>
+        <header>
+
+            <nav>
+                <router-link class="nav-button" to="/">Pesquisa</router-link>
+                <router-link class="nav-button" to="/about">DashBoard</router-link>
+            </nav>
+
+            <router-link to="/login" class="btn-auth nav-button">Entrar</router-link>
+            {/* <router-link to="/" class="btn-auth">Sair</router-link> */}
+
+        </header>
+    </div>
 };
 
 export default Header;
